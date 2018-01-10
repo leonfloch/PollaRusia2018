c2799e5a8885f13a7939aa5e92e5dc2309119eed
DOMASESOFTW\lvalbuena <lvalbuena@asesoftware.com>
2018-01-10 15:40:09 -0500

    demo login

---
+++ src/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'selector-name',
+    templateUrl: 'login.html'
+})
+
+export class Login implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
